1730628b1f340309e6c841075424491c15bf0aba
Silvio Jaureguibehere <silvio.jaureguibehere@gmail.com>
2017-06-23 09:34:06 -0300

    version 0.1.0 sdk 0.3.0

---
+++ PlaywireAdmobAdapter.framework/Headers/PWAdmobCustomEventInterstitial.h
@@ -0,0 +1,21 @@
+//
+//  PWAdmobCustomEventInterstitial.h
+//  playwire-admob
+//
+//  Created by Silvio Jaureguibehere on 5/30/17.
+//  Copyright © 2017 Playwire. All rights reserved.
+//
+
+#ifndef PWAdmobCustomEventInterstitial_h
+#define PWAdmobCustomEventInterstitial_h
+
+
+#endif /* PWAdmobCustomEventInterstitial_h */
+
+@import Foundation;
+@import GoogleMobileAds;
+@import PlaywireSDK;
+
+@interface  PWAdmobCustomEventInterstitial : NSObject<GADCustomEventInterstitial, PWAdManagerDelegate>
+@end
+
---
+++ PlaywireAdmobAdapter.framework/Headers/PlaywireAdmobAdapter.h
@@ -0,0 +1,19 @@
+//
+//  PlaywireAdmobAdapter.h
+//  PlaywireAdmobAdapter
+//
+//  Created by Silvio Jaureguibehere on 6/9/17.
+//  Copyright © 2017 Playwire. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PlaywireAdmobAdapter.
+FOUNDATION_EXPORT double PlaywireAdmobAdapterVersionNumber;
+
+//! Project version string for PlaywireAdmobAdapter.
+FOUNDATION_EXPORT const unsigned char PlaywireAdmobAdapterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PlaywireAdmobAdapter/PublicHeader.h>
+
+#import "PWAdmobCustomEventInterstitial.h"
